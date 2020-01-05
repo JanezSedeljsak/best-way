4a1acdc4bb1dec80af647eecc40a4c05de4c0459
JanezTheProgramer <43420276+JanezTheProgramer@users.noreply.github.com>
2020-01-05 16:04:44 +0100

    base updates

--- public/base.js
+++ public/base.js
@@ -6,5 +6,27 @@ app.controller("myCtrl", [
     "$scope",
     "$window",
     function ($scope, $window) {
+        fetch(`${window.api}/cities`)
+            .then(response => response.json())
+            .then(json => {
+                $scope.possibleCities = json.result.map(x => x.title);
+            });
+        $scope.start = "";
+        $scope.end = "";
+        $scope.valid = [false, false];
+        $scope.validate = function(loc, indx) {
+            debugger;
+            $scope.valid[indx] = $scope.possibleCities.includes(loc);
+        };
+
+        $scope.search = function() {
+            debugger;
+            let validLocations = $scope.validate();
+            console.log(
+                validLocations, 
+                $scope.possibleCities.includes($scope.start), 
+                $scope.possibleCities.includes($scope.end)
+            );
+        }
     }
 ]);
